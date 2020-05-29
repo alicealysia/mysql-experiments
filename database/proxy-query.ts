2f010fe62f4d49e5bde98ba511a32eed9472e1fa
alicealysia <35021050+alicealysia@users.noreply.github.com>
2020-05-29 12:38:57 +1000

    Add files via upload

--- database/proxy-query.ts
+++ database/proxy-query.ts
@@ -3,44 +3,71 @@ import {Table} from './names';
 
 export class Query <tbl extends keyof Table, params extends Table[tbl]> {
     private table: tbl;
-    private paramList: params = {} as params;
+    private paramList: Partial<params> = {} as params;
     private rows: params[] = [];
     private rawConstraint: string = '';
     private parameters: { [key:string]: Partial<params> }[] = [];
     private constraints: { [key:string]: Partial<params> }[] = [];
+    /**
+     * begin your query
+     * @param table name of the table you wish to query
+     */
     constructor (table: tbl) {
         this.table = table;
     }
+    /**
+     * Set a definite where statement, use this when working with complex constraints
+     * @param parameter the where query you wish to work with
+     */
     public WHERE (parameter: string) {
         this.rawConstraint = parameter;
     }
+    /**
+     * find a single row.
+     * @param params parameters you wish to scope your query by. leave blank for a wildcard.
+     */
     public async findOne (params?: (keyof Partial<params>)[] | keyof Partial<params>) {
         const where = this.setWhere();
         const parameters = params? Array.isArray(params)? params.join(', ') : params.toString(): '*';
-        this.paramList = await query_mysql(`SELECT ${parameters} FROM ${this.table} ${where}`).then(results => results[0]); 
-        return this.paramList;
+        const queryResults: params = await query_mysql(`SELECT ${parameters} FROM ${this.table} ${where}`).then(results => results[0]); 
+        return queryResults;
     }
+    /**
+     * find all rows which match the specified constraints
+     * @param params parameters you wish to scope your query by. leave blank for a wildcard.
+     */
     public async find (params?: keyof Partial<params>[]) {
         const where = this.setWhere();
         const parameters = params? Array.isArray(params)? params.join(', ') : params.toString(): '*';
         this.rows = await query_mysql(`SELECT ${parameters} FROM ${this.table} ${where}`); 
         return this.rows;
     }
+    /**
+     * insert specified parameters, constraints will not be used by this.
+     */
     public async insert () {
         const values = Object.values(this.parameters);
         const sqlPlaceholder = Array(values.length).fill('?').join(', ');
         await query_mysql(`INSERT INTO ${this.table}(${Object.keys(this.parameters)}) VALUES(${sqlPlaceholder})`, values);
     }
+    /**
+     * delete any rows which match the specified constraints.
+     */
     public async delete () {
         const where = this.setWhere();
         await query_mysql(`DELETE FROM ${this.table} ${where}`);
     }
+    /**
+     * update the table with the specified parameters where the specified constraints are matched.
+     */
     public async update () {
         const where = this.setWhere();
         const keys = Object.keys(this.parameters).map(value => `${value} = ?`).join(', ');
         const values = Object.values(this.parameters);
         await query_mysql(`UPDATE ${this.table} SET ${keys} ${where}`, values);
     }
+
+    //the handlers for parameters and constraints.
     private parameterHandler = {
         set: <T extends keyof Partial<params>> (obj: params, prop: T, value: params[T]) => {
             this.parameters.push({
